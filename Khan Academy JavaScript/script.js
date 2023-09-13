2a7af74f31088737b9e8b6d0804b0e507c3aa6b6
Dzhavokhir Khamza <96944796+dzhavokhir@users.noreply.github.com>
2023-09-13 22:48:19 +0500

    chore: not done

---
+++ Khan Academy JavaScript/script.js
@@ -0,0 +1,17 @@
+// making a face with circles
+
+/*
+1st - change the position from left to right, or vise-versa
+2nd - also controls the position but from top to bottom
+3rd and 4th - control the size of the ellipse
+*/
+
+// we call the ellipse as the function.
+
+/*
+The ellipse function helps you to position the circles.
+Add more ellipse to add more circles or shapes to your drawings
+*/
+ellipse(200, 200, 300, 350);
+
+ellipse(212, 150, 30,30)
